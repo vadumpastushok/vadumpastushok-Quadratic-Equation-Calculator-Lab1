cde80b22bcdb84cb7296133d84a2d29d4cd89af7
Vadym <vadum.pastushok@gmail.com>
2023-03-07 05:02:12 +0200

    Revert "added simple console input"
    This reverts commit 56c92e649cb7806487cb52e078f1975f604a106f.

--- Main.java
+++ Main.java
@@ -1,15 +1,6 @@
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("a = ");
-        double a = scanner.nextDouble();
-        System.out.print("b = ");
-        double b = scanner.nextDouble();
-        System.out.print("c = ");
-        double c = scanner.nextDouble();
-        System.out.println(a + " " + b + " " + c);
+        System.out.println("Hello world!");
     }
 }
