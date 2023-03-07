5028d78f076ea476607af80e6ec7c6fcb7ca5e05
Vadym <vadum.pastushok@gmail.com>
2023-03-07 05:13:34 +0200

    added logic of solving quadratic equations

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ public class Main {
         double a = readNumber("a = ");
         double b = readNumber("b = ");
         double c = readNumber("c = ");
+        solveMathEquation(a, b, c);
     }
 
     private static double readNumber(String title) {
@@ -16,6 +17,21 @@ public class Main {
         } catch (Exception e) {
             System.out.println("Input is not valid real number");
             return readNumber(title);
+        }
+    }
+
+    private static void solveMathEquation(Double a, Double b, Double c) {
+        double D = b * b - 4 * a * c;
+
+        if (D < 0) {
+            System.out.println("There are 0 roots");
+        } else if (D == 0) {
+            double x = (-b) / (2 * a);
+            System.out.println("There are 1 root:\nx = " + x);
+        } else {
+            double x1 = (-b + Math.sqrt(D)) / (2 * a);
+            double x2 = (-b - Math.sqrt(D)) / (2 * a);
+            System.out.println("There are 2 roots:\nx1 = " + x1 + "\nx2 = " + x2);
         }
     }
 }
