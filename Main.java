56c92e649cb7806487cb52e078f1975f604a106f
Vadym <vadum.pastushok@gmail.com>
2023-03-07 05:01:08 +0200

    added simple console input

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("a = ");
+        double a = scanner.nextDouble();
+        System.out.print("b = ");
+        double b = scanner.nextDouble();
+        System.out.print("c = ");
+        double c = scanner.nextDouble();
+        System.out.println(a + " " + b + " " + c);
     }
 }
