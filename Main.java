38b2f20b7c29beadabd2dc9e0f6d95c202c3341a
Vadym <vadum.pastushok@gmail.com>
2023-03-07 05:05:19 +0200

    moved console input to function with catching exceptions

--- Main.java
+++ Main.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double a = readNumber("a = ");
+        double b = readNumber("b = ");
+        double c = readNumber("c = ");
+    }
+
+    private static double readNumber(String title) {
+        System.out.print(title);
+        try {
+            Scanner scanner = new Scanner(System.in);
+            return scanner.nextDouble();
+        } catch (Exception e) {
+            System.out.println("Input is not valid real number");
+            return readNumber(title);
+        }
     }
 }
